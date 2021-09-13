1617f312c79f6a0e2f55d445b4ab9d1b279774dd
Walison Lima <82228528+SkyWals@users.noreply.github.com>
2021-09-13 15:31:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,23 @@
 #(em versões acima de 3.6 use isto para instalar o PyAudio)
 #pip install pipwin
 #pipwin install pyaudio
+
+import speech_recognition as sr
+import pyttsx3
+
+audio = sr.Recognizer()
+maquina = pyttsx3.init()
+
+
+try:
+    with sr.Microphone() as source:
+        print('ouvindo...')
+        voz = audio.listen(source)
+        comando = audio.recognize_google(voz, language='pt-BR')
+        comando = comando.lower()
+        if 'mizaki' in comando:
+            print(comando)
+            maquina.say(comando)
+            maquina.runAndWait()
+except:
+    print('Microfone não esta OK')
