a8b5d1bcc379d3eddf2c63a903751d62b4dc38b2
SkyWals <gwalisonls@gmail.com>
2021-09-13 15:15:14 -0300

    atualização

---
+++ main.py
@@ -0,0 +1,7 @@
+#BIBLIOTECAS:
+#pip install SpeechRecognition
+#pip install pyttsx3
+#pip install PyAudio (em versões abaixo de 3.6)
+#(em versões acima de 3.6 use isto para instalar o PyAudio)
+#pip install pipwin
+#pipwin install pyaudio
